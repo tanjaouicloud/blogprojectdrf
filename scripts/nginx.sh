8e751a46d182b8e4974e201eca81fd534ba9364a
tanjaouicloud <100701433+tanjaouicloud@users.noreply.github.com>
2025-05-12 09:50:02 -0700

    new update

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/Project_folder_name/nginx/nginx.conf /etc/nginx/sites-available/fodler_name_where_seetngs.py_is
+sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/fodler_name_where_seetngs.py_is
 sudo ln -s /etc/nginx/sites-available/fodler_name_where_seetngs.py_is /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["Your_Instance_Public_IP"]/' /home/ubuntu/Your_project_folder_name/mysite/settings.py
+sed -i 's/\[]/\["35.181.169.124"]/' /home/ubuntu/blogprojectdrf/mysite/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
